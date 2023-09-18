3740927437caa3dbf00f4efe18d009c6d7ac2166
Alexis Mossello <ammossello2012@gmail.com>
2023-09-18 09:33:38 -0300

    modificaciones en medicos.html, styles y views.

--- mediturnosApp/views.py
+++ mediturnosApp/views.py
@@ -16,6 +16,8 @@ def medicos(request, id_especialidad):
         medicos = [['Carlos López','Pediatria','2001'],['Laura García','Pediatria','2002'],['Diego Fernández','Pediatria','2003']]
     elif id_especialidad == 3:
         medicos = [['María Rodríguez','Traumatologia','3001'],['Pepe Argento','Traumatologia','3002'],['Sofía Torres','Traumatologia','3003']]
+    elif id_especialidad == 0:
+        medicos = [['Juan Perez','Clinica','1001'],['Laura García','Clinica','1002'],['Ana Martinez','Clinica','1003'],['Carlos López','Pediatria','2001'],['Laura García','Pediatria','2002'],['Diego Fernández','Pediatria','2003'],['María Rodríguez','Traumatologia','3001'],['Pepe Argento','Traumatologia','3002'],['Sofía Torres','Traumatologia','3003']]
 
     return render(request,'mediturnosApp/turnos/medicos.html',{"id":id_especialidad,"medicos":medicos})
 
