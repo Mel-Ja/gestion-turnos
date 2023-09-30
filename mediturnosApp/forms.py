0b50ea3a3c9d01393fab767e2377d76d713343ef
Martin Nuñez <martinalejandronuniez@gmail.com>
2023-09-30 18:21:43 -0300

    correccion en el input de fecha, haciendo se que solo se pueda elegir una fecha a partir del dia actual

--- mediturnosApp/forms.py
+++ mediturnosApp/forms.py
@@ -1,4 +1,5 @@
 from django import forms	
+from datetime import date
 	
 class SolicitarTurnoForm(forms.Form):	
 	
@@ -8,13 +9,22 @@ class SolicitarTurnoForm(forms.Form):
 	email = forms.EmailField(label="Email", required=True)
 	especialidad = forms.CharField(label="Especialidad", required=True, widget=forms.TextInput(attrs={'pattern': '[a-zA-ZáéíóúÁÉÍÓÚ\s]+', 'title': 'Ingrese solo letras', 'class': 'solo-letras'}))
 	medico = forms.CharField(label="Medico", required=True, widget=forms.TextInput(attrs={'pattern': '[a-zA-ZáéíóúÁÉÍÓÚ\s]+', 'title': 'Ingrese solo letras', 'class': 'solo-letras'}))
-	fecha = forms.DateField(label="Fecha", required=True, widget=forms.DateInput(attrs={'type': 'date'}))
+	fecha = forms.DateField(
+        label="Fecha",
+        required=True,
+        widget=forms.DateInput(
+            attrs={
+                'type': 'date',
+                'min': str(date.today())  # Establecemos como fecha mínima para elegir al dia de hoy
+            }
+        )
+    )
 	hora = forms.TimeField(label="Hora", required=True, widget=forms.TimeInput(attrs={'type': 'time'}))
 	
 	
     
     
    
-   
+    
     
    
