46859cb41ad965e168fc72ce8473697fc0108fe9
Martin Nuñez <martinalejandronuniez@gmail.com>
2023-09-30 20:30:38 -0300

    correcciones menores en el diseño del formulario v2

--- mediturnosApp/forms.py
+++ mediturnosApp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from datetime import datetime, timedelta
 
 class SolicitarTurnoForm(forms.Form):
-    nombre = forms.CharField(label="Nombre", required=True, widget=forms.TextInput(attrs={'class': 'form-control solo-letras', 'placeholder': 'Ingrese su nombre'})), 
+    nombre = forms.CharField(label="Nombre", required=True, widget=forms.TextInput(attrs={'class': 'form-control solo-letras', 'placeholder': 'Ingrese su nombre'}))
     apellido = forms.CharField(label="Apellido", required=True, widget=forms.TextInput(attrs={'class': 'form-control solo-letras', 'placeholder': 'Ingrese su apellido'}))
     dni = forms.CharField(label="DNI", required=True, widget=forms.TextInput(attrs={'class': 'form-control dni-input', 'pattern': '[0-9]{8}', 'title': 'El DNI debe contener 8 dígitos numéricos', 'maxlength': '8', 'placeholder': 'Ingrese su dni'}))
     email = forms.EmailField(label="Email", required=True, widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese su email'}))
