003d486d5f62268702b45ae1e864686f383b28b0
Martin Nuñez <martinalejandronuniez@gmail.com>
2023-09-30 17:58:19 -0300

    formato correcto en input dni

--- mediturnosApp/forms.py
+++ mediturnosApp/forms.py
@@ -4,7 +4,7 @@ class SolicitarTurnoForm(forms.Form):
 	
 	nombre = forms.CharField(label="Nombre", required=True)
 	apellido = forms.CharField(label="Apellido", required=True)
-	dni = forms.CharField(label="Dni", required=True)
+	dni = forms.CharField(label="Dni", required=True, widget=forms.TextInput(attrs={'pattern': '[0-9]{8}', 'title': 'El DNI debe contener 8 dígitos numéricos'}))
 	email = forms.EmailField(label="Email", required=True)
 	Especialidad = forms.CharField(label="Especialidad", required=True)
 	Medico = forms.CharField(label="Medico", required=True)
