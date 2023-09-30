6ceb78ca72ba5cc953676dc52a53201296131c1a
Martin Nuñez <martinalejandronuniez@gmail.com>
2023-09-30 18:10:09 -0300

    Se agregan restricciones de tipeo para cada campo

--- mediturnosApp/forms.py
+++ mediturnosApp/forms.py
@@ -2,13 +2,17 @@ from django import forms
 	
 class SolicitarTurnoForm(forms.Form):	
 	
-	nombre = forms.CharField(label="Nombre", required=True)
-	apellido = forms.CharField(label="Apellido", required=True)
-	dni = forms.CharField(label="Dni", required=True, widget=forms.TextInput(attrs={'pattern': '[0-9]{8}', 'title': 'El DNI debe contener 8 dígitos numéricos'}))
+	nombre = forms.CharField(label="Nombre", required=True, widget=forms.TextInput(attrs={'pattern': '[a-zA-ZáéíóúÁÉÍÓÚ\s]+', 'title': 'Ingrese solo letras', 'class': 'solo-letras'}))
+	apellido = forms.CharField(label="Apellido", required=True, widget=forms.TextInput(attrs={'pattern': '[a-zA-ZáéíóúÁÉÍÓÚ\s]+', 'title': 'Ingrese solo letras', 'class': 'solo-letras'}))
+	dni = forms.CharField(label="Dni", required=True, widget=forms.TextInput(attrs={'pattern': '[0-9]{8}', 'title': 'El DNI debe contener 8 dígitos numéricos', 'class': 'dni-input'}))
 	email = forms.EmailField(label="Email", required=True)
-	Especialidad = forms.CharField(label="Especialidad", required=True)
-	Medico = forms.CharField(label="Medico", required=True)
+	especialidad = forms.CharField(label="Especialidad", required=True, widget=forms.TextInput(attrs={'pattern': '[a-zA-ZáéíóúÁÉÍÓÚ\s]+', 'title': 'Ingrese solo letras', 'class': 'solo-letras'}))
+	medico = forms.CharField(label="Medico", required=True, widget=forms.TextInput(attrs={'pattern': '[a-zA-ZáéíóúÁÉÍÓÚ\s]+', 'title': 'Ingrese solo letras', 'class': 'solo-letras'}))
 	fecha = forms.DateField(label="Fecha", required=True, widget=forms.DateInput(attrs={'type': 'date'}))
 	hora = forms.TimeField(label="Hora", required=True, widget=forms.TimeInput(attrs={'type': 'time'}))
 	
 	
+    
+    
+   
+    
--- mediturnosApp/static/mediturnosApp/js/app.js
+++ mediturnosApp/static/mediturnosApp/js/app.js
@@ -1,2 +1,15 @@
-console.log("Hola");
+// Validacion del input DNI:
 
+document.addEventListener('input', function (e) {
+    if (e.target.classList.contains('dni-input')) {
+        // Permite solo números en el campo DNI
+        e.target.value = e.target.value.replace(/[^0-9]/g, '');
+    }
+});
+
+document.addEventListener('input', function (e) {
+    if (e.target.classList.contains('solo-letras')) {
+        // Permite solo letras en los campos especificados
+        e.target.value = e.target.value.replace(/[^a-zA-ZáéíóúÁÉÍÓÚ\s]/g, '');
+    }
+});
