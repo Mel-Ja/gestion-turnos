8bd9d37925d5432d18e9b1ab372fc302837c10b2
Luke <lcanepa@buenosaires.gob.ar>
2023-10-05 00:09:49 -0300

    Se agrego el soporte para la conexíon al servidor de base de datos PostgreSQL y las migraciones necesarias para su funcionamiento

--- mediturnos/settings.py
+++ mediturnos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a_=n_g347scyykuz=()6=b=n1wo&x_@p0vzf1lp@=x)i-$d)ae'
+#SECRET_KEY = 'django-insecure-a_=n_g347scyykuz=()6=b=n1wo&x_@p0vzf1lp@=x)i-$d)ae'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,11 +75,21 @@ WSGI_APPLICATION = 'mediturnos.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+#
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': config("DATABASE_HOST"),
+        'PORT': config("DATABASE_PORT"),
+        'NAME': config("DATABASE_NAME"),
+        'USER': config("DATABASE_USER"),
+        'PASSWORD': config("DATABASE_PASSWORD")
     }
 }
 
