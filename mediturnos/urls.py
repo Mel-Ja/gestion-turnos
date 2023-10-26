98e242a53c2553dd787c6cfd8e30274cb4a12dad
Martin Nuñez <martinalejandronuniez@gmail.com>
2023-10-26 04:03:06 -0300

    Se agrega la posibilidad de dar de alta pacientes y solicitar turno, sin estar terminado

--- mediturnos/urls.py
+++ mediturnos/urls.py
@@ -22,10 +22,12 @@ urlpatterns = [
     path('medicos/<int:id_especialidad>/', views.medicos, name="medicos"),
     path('especialidades/', views.especialidades, name="especialidades"),
     path('agenda/', views.agenda, name="agenda"),
-    path('solicitarturno/', views.solicitarturno, name="solicitarturno"),
     path('medicos/alta', views.MedicoCreateView.as_view(), name="medicos-alta"),
     path('medicos/listado', views.MedicoListView.as_view(), name="medicos-listado"),
     path('especialidades/alta', views.EspecialidadCreateView.as_view(), name="especialidad-alta"),
+    path('pacientes/alta', views.PacienteCreateView.as_view(), name="pacientes-alta"),
+    path('pacientes/turnos', views.TurnosCreateView.as_view(), name="solicitar-turno"),
+    path('verificar_dni', views.verificar_dni, name='verificar_dni'),
     
  
     
